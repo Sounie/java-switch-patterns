5c58f36bacf5350d6c1d4170ca05ceab9d6026ba
Stephen Souness <Sounie@users.noreply.github.com>
2022-06-28 15:06:14 +1200

    Condensing switch down to not involve an unnecessary variable.

--- src/main/java/nz/sounie/patterns/TryingSwitchPatterns.java
+++ src/main/java/nz/sounie/patterns/TryingSwitchPatterns.java
@@ -4,21 +4,17 @@ public class TryingSwitchPatterns {
     public String doMatchStuff(Object thing) {
         // Pattern matching switch uses -> arrow syntax
         // Cannot mix and match a literal value alongside pattern
-        String result;
-        switch (thing) {
-            case Integer i -> {
+        return switch (thing) {
+            case Integer i ->
                 // Literal matching
-                result = switch (i) {
+                switch (i) {
                     case 100 -> "Int value 100";
                     case 500 -> "Int value 500";
                     default -> "Hello int " + i;
                 };
 
-            }
-            case Number n -> result = "Some non-int number " + n;
-            default -> result = "Didn't find a match, so defaulting";
-        }
-
-        return result;
+            case Number n ->  "Some non-int number " + n;
+            default -> "Didn't find a match, so defaulting";
+        };
     }
 }
